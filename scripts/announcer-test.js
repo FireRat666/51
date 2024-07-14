39f193edad076697526f198d0865a976aa01907b
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-07-14 15:21:33 +1000

    update announcer-test.js

--- scripts/announcer-test.js
+++ scripts/announcer-test.js
@@ -69,6 +69,9 @@ if(window.isBanter && announceevents === true) {
 // Welcome message for user entering the space
 window.loadDoneCallback = ()=>{
     if(window.isBanter) {
+        const username = (user.name ? user.name : user.id.substr(0, 6));
+        let themessage = "Oh no, you broke everything " + username + " now what will we do"; 
+        speak(themessage);
         console.log("Just testing, this should run after load done");
     };
 };
