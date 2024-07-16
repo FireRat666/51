a05e127f74b34ce0f2505611f24ccfb5fea739d6
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-07-16 21:54:12 +1000

    update firescreen.js

--- scripts/firescreen.js
+++ scripts/firescreen.js
@@ -1,6 +1,10 @@
 // Everyone who helped make this possible, HBR, Vanquish3r, DedZed, Sebek and FireRat, And thank you to everyone who helped test it
 var fireScreenOn = 0;
-let thebuttoncolor = "";
+if (thebuttoncolor) {
+	console.log("thebuttoncolor is already declared maybe?");
+} else {
+	let thebuttoncolor = "";
+}
 let volupcolor = "";
 let voldowncolor = "";
 let IconVolUpUrl = "";
