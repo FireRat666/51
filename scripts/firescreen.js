1f2c160337cd32845aae332fde7881d453e24062
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-07-18 16:28:56 +1000

    update firescreen.js

--- scripts/firescreen.js
+++ scripts/firescreen.js
@@ -814,27 +814,27 @@ function getAttrOrDef(pScript, pAttr, pDefault) {
 // 	}
 // }
 var firstbrowserrun = true;
-function testspaceloadstuff() {
-	if (firstbrowserrun === true) {
-		firstbrowserrun = false;
-		console.log("declaring ascene maybe?");
-		const scene = BS.BanterScene.getInstance();
-		scene.On("unity-loaded", () => {
-			setTimeout(() => { 
-				console.log("enableFireScreen");
-				enableFireScreen();
-			}, 3000);
-			console.log("TEST This should run after unity scene loaded.");
-			console.log("TEST This should run after unity scene loaded. 2");
-			console.log("TEST This should run after unity scene loaded. 3");
-		});
-		console.log("End of testspaceloadstuff?");
+// function testspaceloadstuff() {
+// 	if (firstbrowserrun === true) {
+// 		firstbrowserrun = false;
+// 		console.log("declaring ascene maybe?");
+// 		const scene = BS.BanterScene.getInstance();
+// 		scene.On("unity-loaded", () => {
+// 			setTimeout(() => { 
+// 				console.log("enableFireScreen");
+// 				enableFireScreen();
+// 			}, 3000);
+// 			console.log("TEST This should run after unity scene loaded.");
+// 			console.log("TEST This should run after unity scene loaded. 2");
+// 			console.log("TEST This should run after unity scene loaded. 3");
+// 		});
+// 		console.log("End of testspaceloadstuff?");
 
-	} else { console.log("firstbrowserrun is false")}
+// 	} else { console.log("firstbrowserrun is false")}
 
-}
+// }
 
-testspaceloadstuff()
+// testspaceloadstuff()
 // scene.On("loaded", () => {
 //     console.log("This should when the scene has settled and all objects are enumerated");
 //   })
