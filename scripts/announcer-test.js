a078330fdcf39e1113116bbd0a7dcc7cd4eceeb6
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-07-22 23:34:45 +1000

    update announcer-test.js

--- scripts/announcer-test.js
+++ scripts/announcer-test.js
@@ -140,7 +140,7 @@ function announcerloadtest() {
         username + ", What the hell, you broke everything, it was just working, what did you do? ",
         "Hello, Welcome to the space " + username,
         "What are you doing here " + username,
-        "Your King " + username + " has joined the space.",
+        "Welcome your Highness " + username,
         "Hello " + username + " " + username + " " + username + " " + username + " Failure detected, shutting down",
         "Enjoy your stay " + username
       ];
