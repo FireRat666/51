54f246583e01041a2db07102b206921d13136d44
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-07-22 23:52:53 +1000

    update announcer-test.js

--- scripts/announcer-test.js
+++ scripts/announcer-test.js
@@ -75,13 +75,13 @@ if(window.isBanter) {
     // timevariable = minutes + "" + hours + "" + minutes + "" + (minutes + second) + "" + second;
     // timevariable = minutes + (second * 2) + "" + hours + "" + minutes + (minutes + second) + "" + second + "" + second;
     timevariable = hours + "" + minutes + "" + second;
-    let testuppervar = 11;
-    let testuppervar2 = 12;
+    let testuppervar = 13;
+    let testuppervar2 = 14;
     // console.log("The Time Variable Currently is: " + timevariable);
     let psudorandomvar = PRNGF(timevariable, testuppervar);
     let psudorandomvar2 = PRNGF(timevariable, (testuppervar2));
-    console.log("tprnv:" + psudorandomvar);
-    console.log("tprnv2:" + (psudorandomvar2 - 1));
+    console.log("tprnv1:" + psudorandomvar);
+    console.log("tprnv2:" + (psudorandomvar2));
     // if (something === false) {
     //   clearInterval(timerint);
     // };
