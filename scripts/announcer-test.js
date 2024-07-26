ef1646b17e6e0164d279c563e0130f285c01f27d
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-07-26 13:28:58 +1000

    update announcer-test.js

--- scripts/announcer-test.js
+++ scripts/announcer-test.js
@@ -38,66 +38,67 @@ async function speak(text) {
 //   audio.volume = 0.08;
 // };
 
-// This function upon a user joining will select a random welcome message and call the speak function to say it
-if(window.isBanter) {
-  const now = Date.now();
-  window.userJoinedCallback = async user => {
-    if(Date.now() - now > 15000) {
-      username = (user.name ? user.name : user.id.substr(0, 6));
-      theusersid = user.id;
+// // This function upon a user joining will select a random welcome message and call the speak function to say it
+// if(window.isBanter) {
+//   const now = Date.now();
+//   window.userJoinedCallback = async user => {
+//     if(Date.now() - now > 15000) {
+//       username = (user.name ? user.name : user.id.substr(0, 6));
+//       theusersid = user.id;
 
-      if (username === "Gravxton") {username = "Graviton Fucking Hell"};
-      if (username === "Vanquish3r") {username = "Vanquisher"};
-      if (theusersid === "2567af4ddce8000b887527097fd5bf8a") {username = "The Fishiest Overlord of them all"}; // Dedzed
-      if (theusersid === "4c67af8ae899ea5b8dd6da25566ff3f3") {username = "Boob Works"}; // BobWorks
+//       if (username === "Gravxton") {username = "Graviton Fucking Hell"};
+//       if (username === "Vanquish3r") {username = "Vanquisher"};
+//       if (theusersid === "2567af4ddce8000b887527097fd5bf8a") {username = "The Fishiest Overlord of them all"}; // Dedzed
+//       if (theusersid === "4c67af8ae899ea5b8dd6da25566ff3f3") {username = "Boob Works"}; // BobWorks 
+//       // if (theusersid === "f14cd0a7c028d9e8f1756d76ff450c73") {username = "The Slayer"}; // Divine
 
-      const welcomeMessages = [
-        username + " welcome message blah blah!",
-        username + " Joined your party",
-        username + " has spawned into reality",
-        username + " just showed up, Hold my Head Set",
-        username + " just showed up, Don't let them leave",
-        username + " just showed up, Quick call the police",
-        username + " just showed up, Everyone act normal",
-        username + ", What the hell, you broke everything, it was just working, what did you do?!",
-        username + " has joined, what will they do now?",
-        username + " might be an alien, watch them carefully",
-        username + " just stumbled over their charger",
-        username + " Arrived, Everyone go say hi",
-        "Oh No! " + username + " has spawned into reality",
-        "Oh No! " + username + " Left their toilet seat up",
-        "Oh No! " + username + " is about to run out of battery",
-        "Oh No! " + username + " Has forgotten their own name",
-        "Oh No! " + username + " is running out of gas",
-        "Oh No! " + username + " just farted into reality",
-        "Knock Knock " + username + " is here",
-        "Nobody tell " + username + " Their still in their pyjamas",
-        "Oh No! Hide your sheep " + username + " has joined the space",
-        "Oh No! " + username + " needs a recharge, grab the defib",
-        "Your King " + username + " has joined the space.",
-        "Your Queen " + username + " has joined the space.",
-        "Your Magesty " + username + " has joined the space.",
-        "About time " + username + " has joined the space.",
-        "The rumours are true " + username + " has joined the space",
-        "Bow to your King " + username,
-        "Bow to your Queen " + username,
-        "Here we go again, " + username + " has joined the space.",
-        username + " Has Joined the space, And wants to know if you are hungry girl",
-        username + " was pushed into a portal, quick call the police",
-        username + ", be careful of DedZed the fish overlord"
-        ];
-      let psudorandomvar = GETPRNGF(welcomeMessages.length);
-      let randommessage = welcomeMessages[psudorandomvar];
-      console.log("prnv:" + psudorandomvar);
-      const message = randommessage; 
-      await speak(message);
-      console.log("The Time Variable Currently is: " + timevariable);
-      console.log("USERNAME: " + user.name + " USERID: " + theusersid);
-    } else {
-      console.log("ANNOUNCER: Early User Joined Callback event");
-    }
-  }
-};
+//       const welcomeMessages = [
+//         username + " welcome message blah blah!",
+//         username + " Joined your party",
+//         username + " has spawned into reality",
+//         username + " just showed up, Hold my Head Set",
+//         username + " just showed up, Don't let them leave",
+//         username + " just showed up, Quick call the police",
+//         username + " just showed up, Everyone act normal",
+//         username + ", What the hell, you broke everything, it was just working, what did you do?!",
+//         username + " has joined, what will they do now?",
+//         username + " might be an alien, watch them carefully",
+//         username + " just stumbled over their charger",
+//         username + " Arrived, Everyone go say hi",
+//         "Oh No! " + username + " has spawned into reality",
+//         "Oh No! " + username + " Left their toilet seat up",
+//         "Oh No! " + username + " is about to run out of battery",
+//         "Oh No! " + username + " Has forgotten their own name",
+//         "Oh No! " + username + " is running out of gas",
+//         "Oh No! " + username + " just farted into reality",
+//         "Knock Knock " + username + " is here",
+//         "Nobody tell " + username + " Their still in their pyjamas",
+//         "Oh No! Hide your sheep " + username + " has joined the space",
+//         "Oh No! " + username + " needs a recharge, grab the defib",
+//         "Your King " + username + " has joined the space.",
+//         "Your Queen " + username + " has joined the space.",
+//         "Your Magesty " + username + " has joined the space.",
+//         "About time " + username + " has joined the space.",
+//         "The rumours are true " + username + " has joined the space",
+//         "Bow to your King " + username,
+//         "Bow to your Queen " + username,
+//         "Here we go again, " + username + " has joined the space.",
+//         username + " Has Joined the space, And wants to know if you are hungry girl",
+//         username + " was pushed into a portal, quick call the police",
+//         username + ", be careful of DedZed the fish overlord"
+//         ];
+//       let psudorandomvar = GETPRNGF(welcomeMessages.length);
+//       let randommessage = welcomeMessages[psudorandomvar];
+//       console.log("prnv:" + psudorandomvar);
+//       const message = randommessage; 
+//       await speak(message);
+//       console.log("The Time Variable Currently is: " + timevariable);
+//       console.log("USERNAME: " + user.name + " USERID: " + theusersid);
+//     } else {
+//       console.log("ANNOUNCER: Early User Joined Callback event");
+//     }
+//   }
+// };
 
 // This function uses the current time as an input for the psuedo random number generator
 function GETPRNGF(modulo) {
@@ -110,7 +111,7 @@ function GETPRNGF(modulo) {
   return psudorandomvar
 }
   // Function which takes a seed and an upper value then returns a psuedo random number
-  // THIS FUNCTION SEEMS TO NEVER RETURN THE HIGHEST NUMBER IN THE ARRAY
+  // THIS FUNCTION SEEMS TO HARDLY EVER RETURN THE HIGHEST NUMBER IN THE ARRAY
   function PRNGF(seed, modulo) {
     str = `${(2**31-1&Math.imul(48271,seed))/2**31}`
     .split('')
@@ -153,10 +154,49 @@ if(window.isBanter && announceevents === true) {
   })
 };
 
+// // Welcome message for user entering the space
+// function announcerload() {
+//   if(window.isBanter) {
+//     setTimeout(() => { 
+
+//       username = (user.name ? user.name : user.id.substr(0, 6));
+
+//       theusersid = user.id;
+//       if (theusersid === "2567af4ddce8000b887527097fd5bf8a") {
+//         username = "The Fishiest Overlord of them all"
+//       };
+
+//       const joinMessages = [
+//         username + ", What the hell, you broke everything, it was just working, what did you do? ",
+//         "Hello, Welcome to the space " + username,
+//         "What are you doing here " + username,
+//         "Welcome to [Space Name]! We're never letting you go. Quick, lock the doors!",
+//         "Welcome to [Space Name] Zoo! Please, don't feed the animals. ",
+//         "Welcome aboard! " + username + " We’re so excited to have you with us",
+//         "Welcome " + username + " we Hope you brought your sense of humor!",
+//         "Glad you could join us " + username + " now let’s have some fun!",
+//         "Fasten your seatbelt " + username + " it’s going to be a wild ride.",
+//         "Hi there! It’s great to meet you " + username + " , wait a sec I am not sentient",
+//         "Welcome " + username + " We’re a little weird, a little wacky, but we’re pretty sure you’ll fit right in.",
+//         "Welcome your Highness " + username,
+//         "Hello " + username + " " + username + " " + username + " " + username + " Failure detected, shutting down",
+//         "Enjoy your stay " + username
+//       ];
+  
+//       let randommessage = joinMessages[Math.floor(Math.random() * joinMessages.length)];
+//       speak(randommessage);
+//     }, 8000);
+//     console.log("ANNOUNCER: Loading, Delaying to get User.id");
+//   };
+// };
+
 // Welcome message for user entering the space
-function announcerload() {
-  if(window.isBanter) {
-    setTimeout(() => { 
+function announcerloadtest() {
+  const announcerscene = BS.BanterScene.getInstance();
+  announcerscene.On("user-joined", e => {
+    console.log("ANNOUNCER: Test user joined event")
+    if (e.detail.isLocal) {
+      console.log("This should run once, once user joined event is loaded")
 
       username = (user.name ? user.name : user.id.substr(0, 6));
 
@@ -184,21 +224,67 @@ function announcerload() {
   
       let randommessage = joinMessages[Math.floor(Math.random() * joinMessages.length)];
       speak(randommessage);
-    }, 8000);
-    console.log("ANNOUNCER: Loading, Delaying to get User.id");
-  };
-};
-
-
-function announcerloadtest() {
-  const announcerscene = BS.BanterScene.getInstance();
-  announcerscene.On("user-joined", e => {
-    console.log("ANNOUNCER: Test user joined event")
-    if (e.detail.isLocal) {
-      console.log("This should run once, once user joined event is loaded")
-    }
+    } else {
+      const now = Date.now();
+      if(Date.now() - now > 15000) {
+        username = (user.name ? user.name : user.id.substr(0, 6));
+        theusersid = user.id;
+  
+        if (username === "Gravxton") {username = "Graviton Fucking Hell"};
+        if (username === "Vanquish3r") {username = "Vanquisher"};
+        if (theusersid === "2567af4ddce8000b887527097fd5bf8a") {username = "The Fishiest Overlord of them all"}; // Dedzed
+        if (theusersid === "4c67af8ae899ea5b8dd6da25566ff3f3") {username = "Boob Works"}; // BobWorks 
+        // if (theusersid === "f14cd0a7c028d9e8f1756d76ff450c73") {username = "The Slayer"}; // Divine
+  
+        const welcomeMessages = [
+          username + " welcome message blah blah!",
+          username + " Joined your party",
+          username + " has spawned into reality",
+          username + " just showed up, Hold my Head Set",
+          username + " just showed up, Don't let them leave",
+          username + " just showed up, Quick call the police",
+          username + " just showed up, Everyone act normal",
+          username + ", What the hell, you broke everything, it was just working, what did you do?!",
+          username + " has joined, what will they do now?",
+          username + " might be an alien, watch them carefully",
+          username + " just stumbled over their charger",
+          username + " Arrived, Everyone go say hi",
+          "Oh No! " + username + " has spawned into reality",
+          "Oh No! " + username + " Left their toilet seat up",
+          "Oh No! " + username + " is about to run out of battery",
+          "Oh No! " + username + " Has forgotten their own name",
+          "Oh No! " + username + " is running out of gas",
+          "Oh No! " + username + " just farted into reality",
+          "Knock Knock " + username + " is here",
+          "Nobody tell " + username + " Their still in their pyjamas",
+          "Oh No! Hide your sheep " + username + " has joined the space",
+          "Oh No! " + username + " needs a recharge, grab the defib",
+          "Your King " + username + " has joined the space.",
+          "Your Queen " + username + " has joined the space.",
+          "Your Magesty " + username + " has joined the space.",
+          "About time " + username + " has joined the space.",
+          "The rumours are true " + username + " has joined the space",
+          "Bow to your King " + username,
+          "Bow to your Queen " + username,
+          "Here we go again, " + username + " has joined the space.",
+          username + " Has Joined the space, And wants to know if you are hungry girl",
+          username + " was pushed into a portal, quick call the police",
+          username + ", be careful of DedZed the fish overlord"
+          ];
+        let psudorandomvar = GETPRNGF(welcomeMessages.length);
+        let randommessage = welcomeMessages[psudorandomvar];
+        console.log("prnv:" + psudorandomvar);
+        const message = randommessage; 
+        speak(message);
+        console.log("The Time Variable Currently is: " + timevariable);
+        console.log("USERNAME: " + user.name + " USERID: " + theusersid);
+      };
+    };
   });
 }
+
+announcerloadtest();
+
 // // Welcome message for user entering the space
 // function announcerloadtest() {
 //   const announcerscene = BS.BanterScene.getInstance();
@@ -224,8 +310,7 @@ function announcerloadtest() {
 //   })
 // };
 
-announcerload();
-announcerloadtest();
+// announcerload();
 
 // TEST STUFF
 
