28ab53db0c2c2e8cba3cd9c049db1b223f9b08c4
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-08-09 13:37:54 +1000

    update index.html

---
+++ scripts/bullshcript.js
@@ -0,0 +1,28 @@
+// Try Importing Three JS
+import * as THREE from 'three';
+
+// create a reference to the banter scene
+const scene = BS.BanterScene.getInstance();
+
+scene.On("loaded", () => {
+    console.log("index.html setSceneSettings Loading...");
+    // SetSettings - Set settings for the current space like spawn position, portals, guest access etc.
+    const settings = new BS.SceneSettings();
+    settings.EnableDevTools = true;
+    settings.EnableTeleport = true;
+    settings.EnableForceGrab = false;
+    settings.EnableSpiderMan = false;
+    settings.EnablePortals = true;
+    settings.EnableGuests = true;
+    settings.EnableQuaternionPose = false;
+    settings.EnableControllerExtras = false;
+    settings.EnableFriendPositionJoin = false;
+    settings.EnableDefaultTextures = true;
+    settings.EnableAvatars = true;
+    settings.MaxOccupancy = 30;
+    settings.RefreshRate = 90;
+    // settings.ClippingPlane = new Vector2(0.02, 1500);
+    // settings.SpawnPoint = new Vector4(0, 0.1, 0, 90);
+    scene.SetSettings(settings);
+    console.log("index.html finish setting settings for scene");
+});
