1bda5a88125c777816504f6d02c5382769203d78
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-08-09 14:44:34 +1000

    update index.html and bullshcript.js

--- scripts/bullshcript.js
+++ scripts/bullshcript.js
@@ -1,23 +1,55 @@
 // create a reference to the banter scene
 const scene = BS.BanterScene.getInstance();
 
-    console.log("index.html setSceneSettings Loading...");
-    // SetSettings - Set settings for the current space like spawn position, portals, guest access etc.
-    const settings = new BS.SceneSettings();
-    settings.EnableDevTools = true;
-    settings.EnableTeleport = true;
-    settings.EnableForceGrab = false;
-    settings.EnableSpiderMan = false;
-    settings.EnablePortals = true;
-    settings.EnableGuests = true;
-    settings.EnableQuaternionPose = false;
-    settings.EnableControllerExtras = false;
-    settings.EnableFriendPositionJoin = false;
-    settings.EnableDefaultTextures = true;
-    settings.EnableAvatars = true;
-    settings.MaxOccupancy = 30;
-    settings.RefreshRate = 90;
-    settings.ClippingPlane = new THREE.Vector2(0.02, 1500);
-    settings.SpawnPoint = new THREE.Vector4(0, 0.1, 0, 90);
-    scene.SetSettings(settings);
-    console.log("index.html finish setting settings for scene");
+scene.On("loaded", () => {
+  console.log("index.html setSceneSettings Loading...");
+  // SetSettings - Set settings for the current space like spawn position, portals, guest access etc.
+  const settings = new BS.SceneSettings();
+  settings.EnableDevTools = true;
+  settings.EnableTeleport = true;
+  settings.EnableForceGrab = false;
+  settings.EnableSpiderMan = false;
+  settings.EnablePortals = true;
+  settings.EnableGuests = true;
+  settings.EnableQuaternionPose = false;
+  settings.EnableControllerExtras = false;
+  settings.EnableFriendPositionJoin = false;
+  settings.EnableDefaultTextures = true;
+  settings.EnableAvatars = true;
+  settings.MaxOccupancy = 30;
+  settings.RefreshRate = 90;
+  settings.ClippingPlane = new THREE.Vector2(0.02, 1500);
+  settings.SpawnPoint = new THREE.Vector4(0, 0.1, 0, 90);
+  scene.SetSettings(settings);
+  console.log("index.html finish setting settings for scene");
+});
+
+//   class BanterCrap{
+//     constructor() {
+//       console.log("index.html Constructor Loading.");
+//       if(window.isBanter) { 
+//         // this.LoadAssetBundle();
+//       };
+//     }
+//     async LoadAssetBundle() {
+//       console.log("index.html Asset Bundle Loading...");
+//       const windowsUrl = "https://51.firer.at/windows.banter"; 
+//       // https://firer.at/files/bundles/basic.windows.banter
+//       // https://firer.at/files/bundles/basic.android.banter
+//       // https://51.firer.at/windows.banter
+//       // https://51.firer.at/android.banter
+//       // const osxUrl = null; // Not implemented yet...
+//       // const linuxUrl = null; // Not implemented yet...
+//       const androidUrl = "https://51.firer.at/android.banter";
+//       // const iosUrl = null; // Not implemented yet...
+//       // const vosUrl = null; // Not implemented yet...
+//       // const isScene = true;
+//       // const legacyShaderFix = false;
+//       // const gameObject = new BS.GameObject("MyAssetBundle"); 
+//       // console.log("index.html Asset Bundle Loading go...");
+//       // BS.LoadAssetBundles(androidUrl, windowsUrl); // this doesn't work
+//       // const assetBundle = await gameObject.AddComponent(new BS.BanterAssetBundle(windowsUrl, osxUrl, linuxUrl, androidUrl, iosUrl, vosUrl, isScene, legacyShaderFix));
+//           console.log("index.html Asset Bundle Loaded");
+//     }
+// };
+// const banterstuff = new BanterCrap();
