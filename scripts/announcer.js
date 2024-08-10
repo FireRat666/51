f93716d05a85a313d357d49b73d8cc788757842c
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-08-10 11:42:37 +1000

    update announcer.js

--- scripts/announcer.js
+++ scripts/announcer.js
@@ -13,7 +13,7 @@ async function speak(text) {
   if (readytospeak) {
     readytospeak = false
 
-    const svolume = 0.01;
+    const svolume = 0.001;
     const spitch = 1;
     const smute = false;
     const sloop = false;
