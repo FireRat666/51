a2f6137d82f7ef0fc4f7a78daf2fc908ffad195f
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-09-07 19:36:57 +1000

    update rejecter.js

--- scripts/rejecter.js
+++ scripts/rejecter.js
@@ -3,14 +3,14 @@ let theeventid = 12345;
 const rejecterscene = BS.BanterScene.GetInstance();
 
 async function getuserids(thisusersid) {
-  let stateofticket = (await (await fetch("https://dthingy.firer.at/check-user-event/" + thisusersid + "/" + theeventid + "/")).text());
+  let stateofticket = (await (await fetch(`https://dthingy.firer.at/check-user-event/${thisusersid}/${theeventid}/`)).text());
   if (stateofticket === 'true') {
     console.log("stateofticket = true");
-    await rejecterscene.OpenPage("https://dthingy.firer.at/check-user-event/" + thisusersid + "/" + theeventid + "/");
+    await rejecterscene.OpenPage(`https://dthingy.firer.at/check-user-event/${thisusersid}/${theeventid}/`);
   } else {
     console.log("stateofticket = false");
     // openPage("banter://f.bant.ing/");
-    await rejecterscene.OpenPage("https://dthingy.firer.at/check-code/" + thisusersid + "/" + theeventid + "/");
+    await rejecterscene.OpenPage(`https://dthingy.firer.at/check-code/${thisusersid}/${theeventid}/`);
   }
 };
 
