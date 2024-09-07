8dfd3803b3a277809d5beb241c8c21ff8728d3ac
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-09-07 23:53:57 +1000

    update rejecter.js

--- scripts/rejecter.js
+++ scripts/rejecter.js
@@ -3,14 +3,14 @@ let theeventid = 12345;
 const rejecterscene = BS.BanterScene.GetInstance();
 
 async function checkusersid(thisusersid) {
-  let stateofticket = (await (await fetch(`https://dthingy.firer.at/check-user-event/${thisusersid}/${theeventid}/`)).text());
+  let stateofticket = (await (await fetch(`https://dthingy.firer.at/confirm/${thisusersid}/${theeventid}/`)).text());
   if (stateofticket === 'true') {
     console.log("stateofticket = true");
-    await rejecterscene.OpenPage(`https://dthingy.firer.at/check-user-event/${thisusersid}/${theeventid}/`);
+    await rejecterscene.OpenPage(`https://dthingy.firer.at/confirm/${thisusersid}/${theeventid}/`);
   } else {
     console.log("stateofticket = false");
     // openPage("banter://f.bant.ing/");
-    await rejecterscene.OpenPage(`https://dthingy.firer.at/check-code/${thisusersid}/${theeventid}/`);
+    await rejecterscene.OpenPage(`https://dthingy.firer.at/code/${thisusersid}/${theeventid}/`);
   }
 };
 
@@ -18,4 +18,4 @@ rejecterscene.On("user-joined", e => {
   if (e.detail.isLocal) { checkusersid(e.detail.uid); };
 });
 
-// await scene.OpenPage("https://dthingy.firer.at/check-code/" + scene.localUser.uid + "/" + theeventid + "/");
+// await scene.OpenPage("https://dthingy.firer.at/code/" + scene.localUser.uid + "/" + theeventid + "/");
