e053c868bc61eb6127d01729a694981a373f7c97
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-09-08 10:00:50 +1000

    update rejecter.js

--- scripts/rejecter.js
+++ scripts/rejecter.js
@@ -1,15 +1,13 @@
-let theeventid = 12345;
-
 const rejecterscene = BS.BanterScene.GetInstance();
 
 async function checkusersid(thisusersid) {
+  let theeventid = 12345;
   let stateofticket = (await (await fetch(`https://dthingy.firer.at/confirm/${thisusersid}/${theeventid}/`)).text());
   if (stateofticket === 'true') {
-    console.log("stateofticket = true");
-    await rejecterscene.OpenPage(`https://dthingy.firer.at/confirm/${thisusersid}/${theeventid}/`);
+    openportal01()
+    // await rejecterscene.OpenPage(`https://dthingy.firer.at/confirm/${thisusersid}/${theeventid}/`);
   } else {
-    console.log("stateofticket = false");
-    // openPage("banter://f.bant.ing/");
+    // await rejecterscene.OpenPage("banter://f.bant.ing/");
     await rejecterscene.OpenPage(`https://dthingy.firer.at/code/${thisusersid}/${theeventid}/`);
   }
 };
@@ -17,5 +15,13 @@ async function checkusersid(thisusersid) {
 rejecterscene.On("user-joined", e => {
   if (e.detail.isLocal) { checkusersid(e.detail.uid); };
 });
+
+async function openportal01() {
+  const portalObject = new BS.GameObject("MyPortal");
+  const portal = await portalObject.AddComponent(new BS.BanterPortal("https://firer.at", "0001"));
+  const transform = await portalObject.AddComponent(new BS.Transform());
+  transform.position = new BS.Vector3(10,0,-10);
+  transform.localScale = new BS.Vector3(2,4,1);
+};
 
 // await scene.OpenPage("https://dthingy.firer.at/code/" + scene.localUser.uid + "/" + theeventid + "/");
