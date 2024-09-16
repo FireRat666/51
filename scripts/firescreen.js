1541d3052ce666b568ea468a13e5f0d7a1e497a5
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-09-16 11:46:44 +1000

    update firescreen.js

--- scripts/firescreen.js
+++ scripts/firescreen.js
@@ -1176,7 +1176,7 @@ class handButtonCrap{
 			"document.querySelector('.html5-video-player').setVolume(" + firepercent + ");", }, ]);
 		});
 
-			let firevolupbut = document.getElementById("firevoldownbut");
+			let firevolupbut = document.getElementById("firevolupbut");
 			let butcolour = firevolupbut.getAttribute("color");
 			firevolupbut.setAttribute("color", "#FFFFFF"); 
 			setTimeout(() => {  firevolupbut.setAttribute("color", butcolour); }, 100);
@@ -1208,7 +1208,7 @@ class handButtonCrap{
 			"document.querySelector('.html5-video-player').setVolume(" + firepercent + ");", }, ]);
 		});
 
-			let firevolbut = document.getElementById("firevolupbut");
+			let firevolbut = document.getElementById("firevoldownbut");
 			let butcolour = firevolbut.getAttribute("color");
 			firevolbut.setAttribute("color", "#FFFFFF"); 
 			setTimeout(() => {  firevolbut.setAttribute("color", butcolour); }, 100);
