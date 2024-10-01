cff8e2e42d3b2897ddf6ab5329b19ead5b0b86d5
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-10-01 21:28:31 +1000

    update firescreenv2.js

--- scripts/firescreenv2.js
+++ scripts/firescreenv2.js
@@ -1,4 +1,4 @@
-// SDK2 Based FireScreen, V0.69 Beta 3 -- Thank you Everyone who helped make this possible, HBR, Vanquish3r, DedZed, Sebek, Skizot, Shane and FireRat, And thank you to everyone who helped test it
+// SDK2 Based FireScreen, V0.69 Beta 3.1 -- Thank you Everyone who helped make this possible, HBR, Vanquish3r, DedZed, Sebek, Skizot, Shane and FireRat, And thank you to everyone who helped test it
 // FireScreen Tablet for Screen Casts / live streams with volume controls or a portable browser for any website.
 var thisScriptLocation = `https://51.firer.at/scripts/`; // CHANGE THIS URL IF MAKING A COPY OF THIS SCRIPT AND THE ONES BELOW
 var fireScriptName = `${thisScriptLocation}firescreenv2.js`;
