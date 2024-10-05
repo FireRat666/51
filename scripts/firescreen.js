60977083c763ceb10fdfdc69dd5e37b2969ec157
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-10-06 00:43:20 +1000

    update firescreen.js

--- scripts/firescreen.js
+++ scripts/firescreen.js
@@ -528,7 +528,8 @@ async function firescreenloadstuff() {
 
   const waitForUnity = async () => { while (!firescene.unityLoaded) { await new Promise(resolve => setTimeout(resolve, 500)); } };
   await waitForUnity(); enableFireScreen();
-  
+  console.log("FIRESCREEN: Unity-Loaded");
+
 };
 
 firescreenloadstuff();
