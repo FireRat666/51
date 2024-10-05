961e4d40040bc5001761fbbf4209cad18352f2c9
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-10-05 13:31:19 +1000

    update firescreen.js

--- scripts/firescreen.js
+++ scripts/firescreen.js
@@ -147,7 +147,7 @@ function createFireScreen(p_pos, p_rot, p_sca, p_volume, p_url, p_backdrop, p_ca
       firescreen.setAttribute("sq-rigidbody", "useGravity: false; drag:10; angularDrag:10;");
   };
 
-  let firecollider = createButton("0 0 -0.005", "1.0", "0.55", "#ff0000", null, {"sq-boxcollider": "", "sq-grabbable": "", "scale" : "1.0 0.55 0.05", "enableLock" : false}, null, false, "collider");
+  let firecollider = createButton("0 0 -0.005", "1.0", "0.55", "#ff0000", null, {"sq-boxcollider": "", "sq-grabbable": "", "scale" : "1.0 0.55 0.05", "enableLock" : "false"}, null, false, "collider");
   firescreen.appendChild(firecollider);
 
   // firecollider.makeGameObject();
@@ -327,12 +327,12 @@ function setupBrowsers() {
 		const ColliderScreen = lockToggle.parentElement.children[0];
 		let thisbuttoncolor = TheBrowser.getAttribute("button-color");
     const isLockEnabled = ColliderScreen.getAttribute("enableLock");
-
-    console.log(`lockbutton enableLock: ${ColliderScreen.getAttribute("enableLock")}`);
-
     const newColor = isLockEnabled ? (thisbuttoncolor === "#00FF00" ? "#FFFF00" : thisbuttoncolor) : "#00FF00";
+
+    console.log(`lockbutton enableLock: ${ColliderScreen.getAttribute("enableLock")} newColor: ${newColor}`);
+    
     lockToggle.setAttribute("color", newColor);
-    ColliderScreen.setAttribute("enableLock", !isLockEnabled);
+    ColliderScreen.setAttribute("enableLock", isLockEnabled ? "false" : "true");
     
   });  }, 	});
 
@@ -342,7 +342,7 @@ function setupBrowsers() {
     document.querySelectorAll('.firescreenc').forEach(element => {
 
       const ColliderScreen = element.children[0];
-      if (ColliderScreen.getAttribute("enableLock") == "true") {
+      if (ColliderScreen.getAttribute("enableLock") === "true") {
         ColliderScreen.setAttribute("visible",`${state}`);
         console.log(`true enableLock: ${ColliderScreen.getAttribute("enableLock")}`);
       };
