355fffea4ed461c323402d12e023081dbabfac60
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-11-16 13:54:45 +1100

    update announcer.js, firescreen.js and firescreenscripts.js

--- scripts/firescreen.js
+++ scripts/firescreen.js
@@ -106,7 +106,7 @@ function createFireScreen(p_pos, p_rot, p_sca, p_volume, p_url, p_backdrop, p_ca
 
     thebuttoncolor = p_buttoncolor; fireScreenOn = true;
   // Setup the Announcer only on the first run if enabled
-  if (!window.AnnouncerScriptInitialized && typeof announcerscene === 'undefined') {
+  if (!window.AnnouncerScriptInitialized && !window.FireScriptLoaded) { window.FireScriptLoaded = true;
     window.announcerfirstrun = false;
     console.log("FIRESCREEN: Adding the Announcer Script");
     const announcerscript = document.createElement("script");
